886df463a34d38c1e85cd9dcef549fc4213270d1
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2026-05-08 03:33:31 +0200

    move utils to their own file

--- src/index.d.ts
+++ src/index.d.ts
@@ -9,43 +9,9 @@
 
 /* eslint-disable sonarjs/no-built-in-override */
 
+import type { ISODateTime, KeyToString, Split, _Prettify, StripExtension } from './utils.js';
 /* eslint-disable-next-line unicorn/require-module-specifiers -- required */
 export {};
-
-type ISODate = `${number}${number}${number}${number}-${number}${number}-${number}${number}`;
-type ISOTime = `${number}${number}:${number}${number}:${number}${number}.${number}${number}${number}`;
-type ISODateTime = `${ISODate}T${ISOTime}Z`;
-
-type KeyToString<K extends PropertyKey> = K extends string ? K : K extends number ? `${K}` : never;
-
-type Split<S extends string, SEP extends string | undefined, L extends number = number, Acc extends string[] = []> = SEP extends unknown
-  ? [L] extends [Acc['length']] ? Acc
-    : SEP extends undefined ? [S]
-    : string extends S | SEP ? string[]
-    : SEP extends ''
-      ? S extends `${infer Head}${infer Tail}`
-        ? Split<Tail, SEP, L, [...Acc, Head]>
-        : Acc
-      : S extends `${infer Head}${SEP}${infer Tail}`
-        ? Split<Tail, SEP, L, [...Acc, Head]>
-        : [...Acc, S]
-  : never;
-
-type ProbablyDiscordJs = { client: any };
-
-type ShouldSkip<T> = [T] extends [object]
-  ? T extends ProbablyDiscordJs | GenericFunction | GenericConstructor | { constructor: GenericConstructor }
-    ? true
-    : false
-  : true;
-
-type _Prettify<T, Deep extends boolean, Depth extends unknown[] = []>
-  = ShouldSkip<T> extends true ? T
-  : Depth['length'] extends __MAX_PRETTIFY_DEPTH ? T : {
-    [K in keyof T]: Deep extends true
-      ? _Prettify<T[K], Deep, [...Depth, unknown]>
-      : T[K]
-  } & {};
 
 declare global {
   /** The maximum depth for {@link Prettify}. Change with caution. */
---
+++ src/utils.d.ts
@@ -0,0 +1,41 @@
+
+type ISODate = `${number}${number}${number}${number}-${number}${number}-${number}${number}`;
+type ISOTime = `${number}${number}:${number}${number}:${number}${number}.${number}${number}${number}`;
+export type ISODateTime = `${ISODate}T${ISOTime}Z`;
+
+export type KeyToString<K extends PropertyKey> = K extends string ? K : K extends number ? `${K}` : never;
+
+export type Split<S extends string, SEP extends string | undefined, L extends number = number, Acc extends string[] = []> = SEP extends unknown
+  ? [L] extends [Acc['length']] ? Acc
+    : SEP extends undefined ? [S]
+    : string extends S | SEP ? string[]
+    : SEP extends ''
+      ? S extends `${infer Head}${infer Tail}`
+        ? Split<Tail, SEP, L, [...Acc, Head]>
+        : Acc
+      : S extends `${infer Head}${SEP}${infer Tail}`
+        ? Split<Tail, SEP, L, [...Acc, Head]>
+        : [...Acc, S]
+  : never;
+
+type ProbablyDiscordJs = { client: any };
+type ShouldSkip<T> = [T] extends [object]
+  ? T extends ProbablyDiscordJs | GenericFunction | GenericConstructor | { constructor: GenericConstructor }
+    ? true
+    : false
+  : true;
+
+export type _Prettify<T, Deep extends boolean, Depth extends unknown[] = []>
+  = ShouldSkip<T> extends true ? T
+  : Depth['length'] extends __MAX_PRETTIFY_DEPTH ? T : {
+    [K in keyof T]: Deep extends true
+      ? _Prettify<T[K], Deep, [...Depth, unknown]>
+      : T[K]
+  } & {};
+
+export type StripExtension<T extends string, Ext extends string | undefined>
+  = Ext extends string
+    ? T extends `${infer R}${Ext}` 
+      ? R 
+      : T 
+    : T;
